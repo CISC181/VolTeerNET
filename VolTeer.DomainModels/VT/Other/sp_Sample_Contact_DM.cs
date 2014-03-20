52eb562a82b75fe94e15a6f804636084d7157348
CISC475 <bgibbons@udel.edu>
2014-03-20 14:40:16 -0400

    Telerik Upgrade

---
+++ VolTeer.DomainModels/VT/Other/sp_Sample_Contact_DM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VolTeer.DomainModels.VT.Other
+{
+    public class sp_Sample_Contact_DM
+    {
+        public int ContactID { get; set; }
+        public string ContactName { get; set; }
+
+    }
+}
--- VolTeer.SQLServer/Vol/Tables/tblSampleContactAddr.sql
+++ VolTeer.SQLServer/Vol/Tables/tblSampleContactAddr.sql
@@ -2,5 +2,7 @@
 (
 	[ContactID] INT NOT NULL , 
     [AddrID] INT NOT NULL, 
-    PRIMARY KEY ([ContactID], [AddrID])
+    PRIMARY KEY ([ContactID], [AddrID]), 
+    CONSTRAINT [FK_tblSampleContactAddr_tblContact] FOREIGN KEY ([ContactID]) REFERENCES [vol].[tblSampleContact]([ContactID]), 
+    CONSTRAINT [FK_tblSampleContactAddr_tblAddress] FOREIGN KEY ([AddrID]) REFERENCES [vol].[tblSampleAddress]([AddrID])
 )
