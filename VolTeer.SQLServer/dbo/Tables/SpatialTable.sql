72c8b4048f2130e32398eda3d9c0fbe43b401bd0
SteVwonder <stephen272@gmail.com>
2014-03-24 18:36:05 -0400

    Adding missing sql files

---
+++ VolTeer.SQLServer/dbo/Tables/SpatialTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SpatialTable] (
+    [id]       INT               IDENTITY (1, 1) NOT NULL,
+    [GeogCol1] [sys].[geography] NULL,
+    [GeogCol2] AS                ([GeogCol1].[STAsText]())
+);
+
