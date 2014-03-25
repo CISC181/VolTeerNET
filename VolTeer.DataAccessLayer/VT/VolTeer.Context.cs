1419d59e7ef1a933f44028e83508f725397c42b8
SteVwonder <stephen272@gmail.com>
2014-03-25 16:44:36 -0400

    edited edmx context to fix using statements, build no longer broken

--- VolTeer.DataAccessLayer/VT/VolTeer.Context.cs
+++ VolTeer.DataAccessLayer/VT/VolTeer.Context.cs
@@ -9,11 +9,12 @@
 
 namespace VolTeer.DataAccessLayer.VT
 {
+    
     using System;
+    using System.Data;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    using System.Data.Objects;
-    using System.Data.Objects.DataClasses;
+    using System.Data.Entity.Core.Objects;
     using System.Linq;
     
     public partial class VolTeerEntities : DbContext
