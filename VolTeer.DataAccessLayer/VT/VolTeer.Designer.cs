19874b7fb0fc25d84d8f3526908f1be9db0c5e34
svar <svar@udel.edu>
2014-03-25 13:26:25 -0400

    update tbl_skills edmx

--- VolTeer.DataAccessLayer/VT/VolTeer.Designer.cs
+++ VolTeer.DataAccessLayer/VT/VolTeer.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Kevin\Dropbox\2013-2014--3-Senior Spring\CISC 475\Visual Studio\GITRepos\VolTeerNET\VolTeer.DataAccessLayer\VT\VolTeer.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Shawn Varughese\Documents\GitHub\VolTeerNET\VolTeer.DataAccessLayer\VT\VolTeer.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
