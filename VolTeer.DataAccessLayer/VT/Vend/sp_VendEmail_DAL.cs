155482b75504316eeb8a357f5ee006c0d35ca31a
anthony-ism <anthony@isminc.com>
2014-03-27 14:37:07 -0400

    added email, dm bll, and dam

---
+++ VolTeer.DataAccessLayer/VT/Vend/sp_VendEmail_DAL.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+
+namespace VolTeer.DataAccessLayer.VT.Vend
+{
+    public class sp_VendEmail_DAL
+    {
+
+        #region Select Statements
+        public List<sp_VendEmail_DM> ListEmails(int? EmailID)
+        {
+            List<sp_VendEmail_DM> list = new List<sp_VendEmail_DM>();
+            try
+            {
+                using (VolTeerEntities context = new VolTeerEntities())
+                {
+                    list = (from result in context.sp_Vend_Email_Select(EmailID)
+                            select new sp_VendEmail_DM
+                            {
+                                EmailID = result.EmailID,
+                                EmailAddr = result.EmailAddr,
+                                ActiveFlg = result.ActiveFlg
+                            }).ToList();
+                } // Guaranteed to close the Connection
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+
+            return list;
+        }
+
+        public List<sp_VendEmail_DM> ListEmails()
+        {
+            return ListEmails(null);
+        }
+        #endregion
+
+        #region Insert Statements
+        public int InsertEmailContext(sp_VendEmail_DM InputEmail)
+        {
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var NewEmail = new tblVendEmail
+                {
+                    EmailAddr = InputEmail.EmailAddr,
+                    ActiveFlg = InputEmail.ActiveFlg
+                };
+                context.tblVendEmails.Add(NewEmail);
+                context.SaveChanges();
+                //Return the id of the newly created record
+                return NewEmail.EmailID;
+            }
+        }
+        #endregion
+
+        #region Update Statements
+        public void UpdateEmailContext(sp_VendEmail_DM InputEmail)
+        {
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var existingEmail = context.tblVendEmails.Find(InputEmail.EmailID);
+
+                if (InputEmail != null)
+                {
+                    existingEmail.EmailAddr = InputEmail.EmailAddr;
+                    existingEmail.ActiveFlg = InputEmail.ActiveFlg;
+                    context.SaveChanges();
+                }
+            }
+        }
+        #endregion
+
+        #region Delete Statements
+        public void DeleteEmailContext(sp_VendEmail_DM InputEmail)
+        {
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var VendEmailToRemove = (from n in context.tblVendEmails where n.EmailID == InputEmail.EmailID select n).FirstOrDefault();
+                context.tblVendEmails.Remove(VendEmailToRemove);
+                context.SaveChanges();
+            }
+        }
+        #endregion
+    }
+}
