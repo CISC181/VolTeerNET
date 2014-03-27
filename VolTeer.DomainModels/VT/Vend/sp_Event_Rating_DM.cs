912262978d57ae1df8ce3042067a4e32df7461d8
ktucker8 <ktucker@udel.edu>
2014-03-27 14:29:11 -0400

    Event Rating Domain Model

---
+++ VolTeer.DomainModels/VT/Vend/sp_Event_Rating_DM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VolTeer.DomainModels.VT.Vend
+{
+    public partial class sp_Event_Rating_DM
+    {
+        public int RatingID { get; set; }
+        public Nullable<System.Guid> EventID { get; set; }
+        public Nullable<System.Guid> VolID { get; set; }
+        public Nullable<int> RatingValue { get; set; }
+        public bool ActiveFlg { get; set; }
+
+        public virtual sp_Project_Event_DM sp_Project_Event_DM { get; set; }
+    }
+}
