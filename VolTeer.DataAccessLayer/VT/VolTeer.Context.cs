c85313f12f1aa98246f17bca2a3e42b8a490c796
rhutt <rhutt@udel.edu>
2014-03-26 22:15:17 -0400

    DAL and BLL for Vol_Address

--- VolTeer.DataAccessLayer/VT/VolTeer.Context.cs
+++ VolTeer.DataAccessLayer/VT/VolTeer.Context.cs
@@ -706,15 +706,6 @@ namespace VolTeer.DataAccessLayer.VT
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_Vol_Address_MassUpdate", xML_INParameter);
         }
     
-        public virtual ObjectResult<sp_Vol_Address_Select_Result> sp_Vol_Address_Select(Nullable<int> addrID)
-        {
-            var addrIDParameter = addrID.HasValue ?
-                new ObjectParameter("AddrID", addrID) :
-                new ObjectParameter("AddrID", typeof(int));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_Vol_Address_Select_Result>("sp_Vol_Address_Select", addrIDParameter);
-        }
-    
         public virtual int sp_Vol_Address_Update(Nullable<int> addrId, Nullable<bool> activeFlg, string addrLine1, string addrLine2, string addrLine3, string city, string st, Nullable<int> zip, Nullable<int> zip4)
         {
             var addrIdParameter = addrId.HasValue ?
@@ -781,8 +772,8 @@ namespace VolTeer.DataAccessLayer.VT
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("sp_Vol_Email_Insert", emailAddrParameter);
         }
-
-        public virtual ObjectResult<sp_Vol_Email_Select_Result> sp_Vol_Email_Select(Nullable<System.Guid> emailID)
+    
+        public virtual ObjectResult<sp_Vol_Email_Select_Result> sp_Vol_Email_Select(Nullable<int> emailID)
         {
             var emailIDParameter = emailID.HasValue ?
                 new ObjectParameter("EmailID", emailID) :
@@ -967,6 +958,15 @@ namespace VolTeer.DataAccessLayer.VT
                 new ObjectParameter("EndTime", typeof(System.TimeSpan));
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_Availability_Update", volIDParameter, addrIDParameter, availDateIDParameter, availStartDateParameter, availEndDateParameter, dayIDParameter, startTimeParameter, endTimeParameter);
+        }
+    
+        public virtual ObjectResult<sp_Vol_Address_Select_Result> sp_Vol_Address_Select(Nullable<int> addrID)
+        {
+            var addrIDParameter = addrID.HasValue ?
+                new ObjectParameter("AddrID", addrID) :
+                new ObjectParameter("AddrID", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_Vol_Address_Select_Result>("sp_Vol_Address_Select", addrIDParameter);
         }
     }
 }
