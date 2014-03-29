362dd0144e2d202781a13d394913d5fd79697b74
Ryan O'Dowd <rodowd@udel.edu>
2014-03-29 13:26:02 -0400

    adding geocoding--initial commit

---
+++ VolTeer.ExternalServiceLayer/Interfaces/IGeocoder.cs
@@ -0,0 +1,15 @@
+﻿using VolTeer.DomainModels;
+using VolTeer.DomainModels.Service;
+
+namespace VolTeer.ExternalServiceLayer
+{
+    public interface IGeocoder
+    {
+        /// <summary>
+        /// Attemps to retrieve the lat/lon associated with the given address.
+        /// </summary>
+        /// <param name="address">The address to locate.</param>
+        /// <returns>A <see cref="Coordinate" /> object, or null if the address could not be located.</returns>
+        string GetLatLongFromAddress(GoogleAddress address);
+    }
+}
