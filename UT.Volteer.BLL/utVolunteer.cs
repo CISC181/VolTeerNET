8882b44d21cc075e6943f9b4c14441fad6769f84
CISC475 <bgibbons@udel.edu>
2014-04-08 22:57:26 -0400

    Sync UT

---
+++ UT.Volteer.BLL/utVolunteer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VolTeer.DomainModels.VT.Vol;
+using VolTeer.BusinessLogicLayer.VT.Vol;
+
+namespace UT.Volteer.BLL
+{
+    [TestClass]
+    public class utVolunteer
+    {
+        [TestMethod]
+        public void CreateVolunteer()
+        {
+            sp_Volunteer_DM VOL = new sp_Volunteer_DM();
+            sp_Volunteer_BLL VOlBll = new sp_Volunteer_BLL();
+
+            VOL.VolFirstName = "FNTest";
+            VOL.VolMiddleName = "MNTest";
+            VOL.VolLastName = "LNTest";
+
+            VOlBll.InsertVolunteerContext(VOL);
+
+
+        }
+    }
+}
