df67fa7001203550790114dd26dfa59e3c9db33a
CISC475 <bgibbons@udel.edu>
2014-04-11 00:13:59 -0400

    UT Testing.... with Excel Spreadsheet for Test Data

--- UT.Volteer.BLL/utVolunteer.cs
+++ UT.Volteer.BLL/utVolunteer.cs
@@ -3,31 +3,52 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VolTeer.DomainModels.VT.Vol;
 using VolTeer.BusinessLogicLayer.VT.Vol;
 using UT.Volteer.BLL.HelperMethods;
+using System.Data;
 
 namespace UT.Volteer.BLL
 {
     [TestClass]
     public class utVolunteer
     {
+        const string CWorkbook = "C:\\Users\\Dad\\Source\\Repos\\VolTeerNET\\UT.Volteer.BLL\\UTTestFiles\\Volunteer.xlsx";
+
         [TestMethod]
         public void CreateVolunteer()
         {
-            sp_Volunteer_DM insertVol = new sp_Volunteer_DM();
-            sp_Volunteer_DM selectVol = new sp_Volunteer_DM();
 
-            hVolunteer hVol = new hVolunteer();
-            insertVol = hVol.hCreateVolunteer("New", "Test", "User");
+            DataTable dt = new DataTable();
+            cExcel _cExcel = new cExcel();
+            string strSheetName = "Volunteer";
+
+            dt = _cExcel.ReadExcelFile(strSheetName, CWorkbook);
+
+            foreach (DataRow row in dt.Rows) // Loop over the rows.
+            {
+
+                Console.WriteLine("--- Row ---"); // Print separator.
+                string strFirstName = row["FirstName"].ToString();
+                string strMiddleName = row["MiddleName"].ToString();
+                string strLastName = row["LastName"].ToString();
+                
+                sp_Volunteer_DM insertVol = new sp_Volunteer_DM();
+                sp_Volunteer_DM selectVol = new sp_Volunteer_DM();
+
+                hVolunteer hVol = new hVolunteer();
+                insertVol = hVol.hCreateVolunteer(strFirstName,strMiddleName,strLastName);
 
 
-            selectVol = hVol.hSelectVolunteer(insertVol.VolID);
+                selectVol = hVol.hSelectVolunteer(insertVol.VolID);
 
-            Assert.AreEqual(insertVol.VolFirstName, selectVol.VolFirstName);
-            Assert.AreEqual(insertVol.VolMiddleName, selectVol.VolMiddleName);
-            Assert.AreEqual(insertVol.VolLastName, selectVol.VolMiddleName);
+                Assert.AreEqual(insertVol.VolFirstName, selectVol.VolFirstName);
+                Assert.AreEqual(insertVol.VolMiddleName, selectVol.VolMiddleName);
+                Assert.AreEqual(insertVol.VolLastName, selectVol.VolLastName);
 
-            
-            Console.WriteLine(insertVol.VolID);
-            System.Diagnostics.Debug.Write(insertVol.VolID);
+                Console.WriteLine(insertVol.VolID);
+                System.Diagnostics.Debug.Write(insertVol.VolID);
+            }
+
+
+
         }
     }
 }
