e872ccd006c647b375260370d6e48e975f7dedbc
CISC475 <markyu@udel.edu>
2014-04-14 09:39:00 -0400

    VolPhone BLL DAL DM

---
+++ VolTeer.DomainModels/VT/Vol/sp_Phone_DM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VolTeer.DomainModels.VT.Vol
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class sp_Phone_DM
+    {
+        public int PhoneID { get; set; }
+        public System.Guid VolID { get; set; }
+        public string PhoneNbr { get; set; }
+        public Nullable<bool> ActiveFlg { get; set; }
+
+        
+    }
+}
