75470b807a070f0cd360fe96e77077bfca5d7ab4
rhutt <rhutt@udel.edu>
2014-04-28 18:28:00 -0400

    added the contracts for VolPhone and VolEmail

---
+++ VolTeer.Contracts/VT/Vol/sp_VolPhone_CON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vol;
+
+namespace VolTeer.Contracts.VT.Vol
+{
+    public interface sp_VolPhone_CON
+    {
+        List<sp_Phone_DM> ListPhones(sp_Phone_DM cVolPhone);
+        sp_Phone_DM ListPrimaryPhone(sp_Phone_DM cVolPhone);
+        void InsertPhoneContext(sp_Phone_DM _cPhone);
+        void UpdatePhoneNbr(sp_Phone_DM _cPhone);
+        void DeletePhonesContext(sp_Phone_DM _cPhone);
+    }
+}
