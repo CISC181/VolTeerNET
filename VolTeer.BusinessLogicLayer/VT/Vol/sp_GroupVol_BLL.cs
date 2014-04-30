4fd03c1de3c8fa9fff6a35a15158f8c665217e45
bkarrer <bkarrer@udel.edu>
2014-04-30 15:58:53 -0400

    commit

--- VolTeer.BusinessLogicLayer/VT/Vol/sp_GroupVol_BLL.cs
+++ VolTeer.BusinessLogicLayer/VT/Vol/sp_GroupVol_BLL.cs
@@ -10,14 +10,32 @@ namespace VolTeer.BusinessLogicLayer.VT.Vol
 {
     public class sp_GroupVol_BLL
     {
+        sp_GroupVol_DAL DAL = new sp_GroupVol_DAL();
+
 
         //TODO: Create an insert method passing in domain model
+        public sp_Vol_GroupVol_DM InsertGroupContext(ref sp_Vol_GroupVol_DM _cGroup)
+        {
+            return DAL.InsertGroupContext(ref _cGroup);
+        }
 
-        //TODO: Create a deleteall method passing in VolID
+        //TODO: Create a delete all method passing in VolID
+        public void DeleteGroupContext(sp_Vol_GroupVol_DM _cVolID)
+        {
+            DAL.DeleteGroupContext(_cVolID);
+        }
+
 
         //TODO: Create a MakePrimary method passing in VolID
+        public void MakePrimaryVolID(sp_Vol_GroupVol_DM _cGroup)
+        {
+            DAL.MakeAdminVolID(_cGroup);
+        }
 
         //TODO: Create a Make Admin method passing in VolID
-
+        public void MakeAdminVolID(sp_Vol_GroupVol_DM _cGroup)
+        {
+            DAL.MakeAdminVolID(_cGroup);
+        }
     }
 }
