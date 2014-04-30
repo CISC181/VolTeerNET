6447d3962bfe2f14602fba9f6675133094567398
zhaden <andrewdimola@gmail.com>
2014-04-29 22:36:10 -0400

    Fixed Vendor Update Error

--- VolTeer.DataAccessLayer/VT/VolTeer.Designer.cs
+++ VolTeer.DataAccessLayer/VT/VolTeer.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Aditi\Source\Repos\VolTeerNET2\VolTeer.DataAccessLayer\VT\VolTeer.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\zhaden\Source\Repos\VolTeerNET\VolTeer.DataAccessLayer\VT\VolTeer.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
