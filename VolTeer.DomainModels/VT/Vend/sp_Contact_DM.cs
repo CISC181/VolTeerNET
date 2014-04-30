ea93aae2344f898336b3572a96062cf90219d875
Duke Park <biglake@udel.edu>
2014-04-30 00:59:21 -0400

    Adding DAL DM for Contact

---
+++ VolTeer.DomainModels/VT/Vend/sp_Contact_DM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VolTeer.DomainModels.VT.Vend
+{
+    public partial class sp_Contact_DM
+    {
+        public sp_Contact_DM()
+        {
+
+        }
+
+        public Guid ContactID { get; set; }
+        public string ContactFirstName { get; set; }
+        public string ContactMiddleName { get; set; }
+        public string ContactLastName { get; set; }
+        public bool ActiveFlg { get; set; }
+    }
+}
