695e74640d7eba2c38e337118b126ce93e0be933
CISC475 <bgibbons@ude.edu>
2014-05-03 23:47:32 -0400

    Committing Vend project

---
+++ Vend/Masters/Site.Master.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vend.Masters
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {                
+                if (HttpContext.Current.Request.IsAuthenticated)
+                {
+                    
+
+                    RadMenu1.Items[1].Items[0].Visible = false;
+                    RadMenu1.Items[1].Items[1].Visible = false;
+                    RadMenu1.Items[1].Items[2].Visible = true;
+                    RadMenu1.Items[1].Items[3].Visible = true;
+                    RadMenu1.Items[1].Items[4].Visible = false;
+
+                    RadMenu1.Height = 48;
+                }
+                else
+                {
+                    RadMenu1.Items[1].Items[0].Visible = true;
+                    RadMenu1.Items[1].Items[1].Visible = true;
+                    RadMenu1.Items[1].Items[2].Visible = false;
+                    RadMenu1.Items[1].Items[3].Visible = false;
+                    RadMenu1.Items[1].Items[4].Visible = true;
+
+                    RadMenu1.Height = 80;
+                }
+//                RadMenu1.Height = rHeight;
+
+            }
+
+        }
+    }
+}
