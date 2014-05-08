4da778f8327852843f7ccf2db4a3b2424912d4f8
pdasa <pdasa@outlook.com>
2014-05-08 17:33:17 -0400

    Added DAL DM CON for ProjectEvent

--- VolTeer.DomainModels/VT/Vend/sp_ProjectEvent_DM.cs
+++ VolTeer.DomainModels/VT/Vend/sp_ProjectEvent_DM.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace VolTeer.DomainModels.VT.Vend
 {
     public partial class sp_ProjectEvent_DM
     {
+        public sp_ProjectEvent_DM()
+        {
+
+        }
+
+        public Guid EventID { get; set; }
+        public Guid ProjectID { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public int AddrID { get; set; }
     }
 }
