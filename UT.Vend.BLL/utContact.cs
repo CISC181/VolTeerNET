13b38035c74088b188179045096dce0cf52ba3c1
anthony-ism <anthony@isminc.com>
2014-05-07 21:51:02 -0400

    contact and vendor ut
    abstracted test method, setup and tear down, fixed excel files to run
    properly

---
+++ UT.Vend.BLL/utContact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VolTeer.DomainModels.VT.Vend;
+using VolTeer.BusinessLogicLayer.VT.Vend;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.EntityClient;
+using System.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using UT.Helper;
+
+using System.Linq;
+
+namespace UT.Vend.BLL
+{
+    [TestClass]
+    public class utContact
+    {
+        static string[] ExcelFilenames = new string[] {
+            "Contact.xlsx", "VendEmail.xlsx", "ContactEmail.xlsx"
+        };
+
+       
+
+      
+
+        [ClassInitialize]
+        public static void InsertContactData(TestContext testContext)
+        {
+            cExcel.InsertData(ExcelFilenames);
+            
+        }
+
+        [TestMethod]
+        public void TestContact()
+        {
+        }
+
+        [ClassCleanup]
+        public static void RemoveContactData()
+        {
+            cExcel.RemoveData(ExcelFilenames);
+        }
+    }
+}
