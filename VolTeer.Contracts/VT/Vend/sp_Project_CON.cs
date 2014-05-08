11fdabb601c436e83deb1693d58507cb0299958a
kcorder <kcorder@udel.edu>
2014-05-08 16:19:11 -0400

    added DAL,DM,CON for Vend.tblProject

---
+++ VolTeer.Contracts/VT/Vend/sp_Project_CON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+
+namespace VolTeer.Contracts.VT.Vend
+{
+    public interface sp_Project_CON
+    {
+        List<sp_Project_DM> ListProjects();
+        sp_Project_DM ListProjects(Guid? ProjectID);
+        sp_Project_DM InsertProjectContext(ref sp_Project_DM _cProject);
+        void UpdateVolunteerContext(sp_Project_DM _cProject);
+        void DeleteProjectContext(sp_Project_DM _cProject);
+    }
+}
--- VolTeer.DomainModels/VT/Vend/sp_Project_DM.cs
+++ VolTeer.DomainModels/VT/Vend/sp_Project_DM.cs
@@ -7,5 +7,10 @@ namespace VolTeer.DomainModels.VT.Vend
 {
     public class sp_Project_DM
     {
+        public System.Guid ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDesc { get; set; }
+        public Nullable<int> AddrID { get; set; }
+        public Nullable<bool> ActiveFlg { get; set; }
     }
 }
