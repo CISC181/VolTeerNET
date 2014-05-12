2d216117418d12beb4ae7da91d234b61d10e4dc4
Duke Park <biglake@udel.edu>
2014-05-12 14:02:58 -0400

    Just adding file....didn really do anything

--- VolTeer.BusinessLogicLayer/VT/Vend/sp_Contact_BLL.cs
+++ VolTeer.BusinessLogicLayer/VT/Vend/sp_Contact_BLL.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+using VolTeer.DataAccessLayer.VT.Vend;
 
 namespace VolTeer.BusinessLogicLayer.VT.Vend
 {
     class sp_Contact_BLL
     {
-        
     }
 }
