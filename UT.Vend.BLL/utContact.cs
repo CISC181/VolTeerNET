d23df98a119f4108e3cfb9bef0fe72465ca4196d
anthony-ism <anthony@isminc.com>
2014-05-12 07:47:34 -0400

    changes to utContact

--- UT.Vend.BLL/utContact.cs
+++ UT.Vend.BLL/utContact.cs
@@ -33,7 +33,7 @@ namespace UT.Vend.BLL
         }
 
         [TestMethod]
-        public void TestContact()
+        public void TestContactRead()
         {
             //Test Our Read
             DataTable dt = cExcel.ReadExcelFile("Sheet1", Path.Combine(cExcel.GetHelperFilesDir(), "Contact.xlsx"));
@@ -42,10 +42,72 @@ namespace UT.Vend.BLL
                 string contactID = row["ContactID"].ToString();
                 sp_VendContact_BLL contact = new sp_VendContact_BLL();
                 sp_VendContact_DM data = contact.ListContacts(new Guid(contactID));
-
+                Assert.AreEqual(row["ContactFirstName"].ToString(), data.ContactFirstName, "Contact First Name Not Set As Expected");
+                Assert.AreEqual(row["ContactMiddleName"].ToString(), data.ContactMiddleName, "Contact Middle Name Not Set As Expected");
+                Assert.AreEqual(row["ContactLastName"].ToString(), data.ContactLastName, "Contact Last Name Not Set As Expected");
             }
-
         }
+
+        [TestMethod]
+        public void TestContactUpdate()
+        {
+            //Test Our Read
+            DataTable dt = cExcel.ReadExcelFile("Sheet1", Path.Combine(cExcel.GetHelperFilesDir(), "Contact.xlsx"));
+            foreach (DataRow row in dt.Rows) // Loop over the rows.
+            {
+                string updateFirstName = "TestFirstName";
+                string updateMiddleName = "TestMiddleName";
+                string updateLastName = "TestLastName";
+                string contactID = row["ContactID"].ToString();
+                sp_VendContact_DM data = new sp_VendContact_DM();
+                data.ContactID = new Guid(contactID);
+                data.ContactFirstName = updateFirstName;
+                data.ContactMiddleName = updateMiddleName;
+                data.ContactLastName = updateLastName;
+                sp_VendContact_BLL contact = new sp_VendContact_BLL();
+                contact.UpdateContactContext(data);
+                data = contact.ListContacts(new Guid(contactID));
+                Assert.AreEqual(updateFirstName, data.ContactFirstName, "Contact First Name Not Set As Expected");
+                Assert.AreEqual(updateMiddleName, data.ContactMiddleName, "Contact Middle Name Not Set As Expected");
+                Assert.AreEqual(updateLastName, data.ContactLastName, "Contact Last Name Not Set As Expected");
+                
+            }
+        }
+
+        [TestMethod]
+        public void TestContactDelete()
+        {
+            //Test Our Read
+            DataTable dt = cExcel.ReadExcelFile("Sheet1", Path.Combine(cExcel.GetHelperFilesDir(), "Contact.xlsx"));
+            foreach (DataRow row in dt.Rows) // Loop over the rows.
+            {
+                string contactID = row["ContactID"].ToString();
+                sp_VendContact_DM data = new sp_VendContact_DM();
+                data.ContactID = new Guid(contactID);
+                sp_VendContact_BLL contact = new sp_VendContact_BLL();
+                contact.DeleteContactContext(data);
+                data = contact.ListContacts(new Guid(contactID));
+                Assert.AreEqual(false, data.ActiveFlg, "ActiveFlag not set as expected");
+            }
+        }
+
+        [TestMethod]
+        public void TestContactInsert()
+        {
+            //Test Our Read
+            DataTable dt = cExcel.ReadExcelFile("Sheet1", Path.Combine(cExcel.GetHelperFilesDir(), "Contact.xlsx"));
+            foreach (DataRow row in dt.Rows) // Loop over the rows.
+            {
+                string contactID = row["ContactID"].ToString();
+                sp_VendContact_DM data = new sp_VendContact_DM();
+                data.ContactID = new Guid(contactID);
+                sp_VendContact_BLL contact = new sp_VendContact_BLL();
+                contact.DeleteContactContext(data);
+                data = contact.ListContacts(new Guid(contactID));
+                Assert.AreEqual(false, data.ActiveFlg, "ActiveFlag not set as expected");
+            }
+        }
+
 
         [ClassCleanup]
         public static void RemoveContactData()
