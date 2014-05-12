a634ccfd9f21fe7e63cface23ead4485b0e09f5d
Duke Park <biglake@udel.edu>
2014-05-12 13:43:02 -0400

    i dont know what im doing

---
+++ VolTeer.BusinessLogicLayer/VT/Vend/sp_Contact_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VolTeer.BusinessLogicLayer.VT.Vend
+{
+    class sp_Contact_BLL
+    {
+        
+    }
+}
