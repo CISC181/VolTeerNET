a4b9149f09e936fa53caccca9179ad8ff92094c0
Duke Park <biglake@udel.edu>
2014-05-13 15:24:06 -0400

    I didn't broke the build

--- VolTeer.DataAccessLayer/VT/Vend/sp_ContactEmail_DAL.cs
+++ VolTeer.DataAccessLayer/VT/Vend/sp_ContactEmail_DAL.cs
@@ -38,35 +38,46 @@ namespace VolTeer.DataAccessLayer.VT.Vend
         #region Insert Statements
         public void insert(sp_ContactEmail_DM contactemail)
         {
-            //using (VolTeerEntities context = new VolTeerEntities())
-            //{
-            //    context.sp_ContactEmail_Insert(contactemail.ContactID, contactemail.EmailID, contactemail.PrimaryEmail);
-            //    context.SaveChanges();
-            //}
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var NewContactEmail = new tblContactEmail
+                {
+                    ContactID = contactemail.ContactID,
+                    EmailID = contactemail.EmailID,
+                    PrimaryEmail = contactemail.PrimaryEmail
+                };
+                context.tblContactEmails.Add(NewContactEmail);
+                context.SaveChanges();
+            }
         }
         #endregion
 
         #region Update Statements
         public void update(sp_ContactEmail_DM contactemail)
         {
-            //using (VolTeerEntities context = new VolTeerEntities())
-            //{
-            //    context.sp_ContactEmail_Update(
-            //        contactemail.ContactID, contactemail.EmailID, contactemail.PrimaryEmail);
-            //    context.SaveChanges();
-            //}
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var ExistingContactEmail = context.tblContactEmails.Find(contactemail.ContactID, contactemail.EmailID);
+                if (ExistingContactEmail != null)
+                {
+                    ExistingContactEmail.PrimaryEmail = contactemail.PrimaryEmail;
+                    context.SaveChanges();
+                }
+            }
         }
         #endregion
 
         #region Delete Statements
         public void delete(sp_ContactEmail_DM contactemail)
         {
-            //using (VolTeerEntities context = new VolTeerEntities())
-            //{
-            //    context.sp_ContactEmail_Delete(
-            //        contactemail.ContactID, contactemail.EmailID);
-            //    context.SaveChanges();
-            //}
+            using (VolTeerEntities context = new VolTeerEntities())
+            {
+                var ContactEmailtoRemove = context.tblContactEmails.Find(contactemail.ContactID, contactemail.EmailID);
+                if (ContactEmailtoRemove != null) {
+                    context.tblContactEmails.Remove(ContactEmailtoRemove);
+                    context.SaveChanges();
+                }
+            }
         }
         #endregion
     }
