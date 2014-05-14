466c3349e4af6eecc8596070d38a5c89a8cca4c4
pdasa <pdasa@outlook.com>
2014-05-14 01:42:36 -0400

    Added VendorProjContact DM, CON, DLL, BLL

---
+++ VolTeer.BusinessLogicLayer/VT/Vend/sp_VendorProjContact_BLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+using VolTeer.DataAccessLayer.VT.Vend;
+
+namespace VolTeer.BusinessLogicLayer.VT.Vend
+{
+    public class sp_VendorProjContact_BLL
+    {
+        sp_VendorProjContact_DAL DAL = new sp_VendorProjContact_DAL();
+
+        public List<sp_VendorProjContact_DM> ListContact()
+        {
+            return DAL.ListContact();
+        }
+
+        public sp_VendorProjContact_DM ListContact(Guid VendorID, Guid ProjectID, Guid ContactID)
+        {
+            return DAL.ListContact(VendorID, ProjectID, ContactID).Single();
+        }
+
+        public Guid InsertContactContext(sp_VendorProjContact_DM InputContact)
+        {
+            return DAL.InsertContactContext(InputContact);
+        }
+
+        public void UpdateContactContext(sp_VendorProjContact_DM InputContact)
+        {
+            DAL.UpdateContactContext(InputContact);
+        }
+
+        public void DeleteContactContext(sp_VendorProjContact_DM InputContact)
+        {
+            DAL.DeleteContactContext(InputContact);
+        }
+    }
+}
