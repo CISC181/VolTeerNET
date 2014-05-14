c078cae2e23b930ecda23e13daa982c896c4996b
pdasa <pdasa@outlook.com>
2014-05-14 00:54:13 -0400

    Added VendorAddr DM, CON, BLL, DLL

---
+++ VolTeer.Contracts/VT/Vend/sp_VendorAddr_CON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+
+namespace VolTeer.Contracts.VT.Vend
+{
+    public interface sp_VendorAddr_CON
+    {
+        List<sp_VendorAddr_DM> ListAddresses(Guid? VendorID);
+        List<sp_VendorAddr_DM> ListAddresses();
+        int InsertAddressContext(sp_VendorAddr_DM InputAddress);
+        void UpdateAddressContext(sp_VendorAddr_DM InputAddress);
+        void DeleteAddressContext(sp_VendorAddr_DM InputAddress);
+    }
+}
