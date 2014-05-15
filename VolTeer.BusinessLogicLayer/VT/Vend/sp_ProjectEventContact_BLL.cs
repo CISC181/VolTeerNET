eeff23efe9ca81ff5b2bb7f5dfbbded6bc4cbc8f
kcorder <kcorder@udel.edu>
2014-05-15 01:56:34 -0400

    added BLL, DAL, DM and CON for ProjectEventContact

---
+++ VolTeer.BusinessLogicLayer/VT/Vend/sp_ProjectEventContact_BLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.Contracts.VT.Vend;
+using VolTeer.DomainModels.VT.Vend;
+using VolTeer.DataAccessLayer.VT.Vend;
+
+namespace VolTeer.BusinessLogicLayer.VT.Vend 
+{
+    public class sp_ProjectEventContact_BLL : sp_ProjectEventContact_CON
+    {
+        sp_ProjectEventContact_DAL dal = new sp_ProjectEventContact_DAL();
+
+        public List<DomainModels.VT.Vend.sp_ProjectEventContact_DM> ListEventsContacts(Guid? EventID, Guid? ContactID)
+        {
+            return dal.ListEventsContacts(EventID, ContactID);
+        }
+
+        public List<DomainModels.VT.Vend.sp_ProjectEventContact_DM> ListEventsContacts()
+        {
+            return dal.ListEventsContacts();
+        }
+
+        public Guid InsertProjectEventContactContext(DomainModels.VT.Vend.sp_ProjectEventContact_DM InputProjectEventContact)
+        {
+            return dal.InsertProjectEventContactContext(InputProjectEventContact);
+        }
+
+        public void UpdateProjectEventContactContext(DomainModels.VT.Vend.sp_ProjectEventContact_DM InputProjectEventContact)
+        {
+            dal.UpdateProjectEventContactContext(InputProjectEventContact);
+        }
+
+        public void DeleteProjectEventContactContext(DomainModels.VT.Vend.sp_ProjectEventContact_DM InputProjectEventContact)
+        {
+            dal.DeleteProjectEventContactContext(InputProjectEventContact);
+        }
+    }
+}
---
+++ VolTeer.Contracts/VT/Vend/sp_ProjectEventContact_CON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VolTeer.DomainModels.VT.Vend;
+
+namespace VolTeer.Contracts.VT.Vend
+{
+    public interface sp_ProjectEventContact_CON
+    {
+        List<sp_ProjectEventContact_DM> ListEventsContacts(Guid? EventID, Guid? ContactID);
+        List<sp_ProjectEventContact_DM> ListEventsContacts();
+        Guid InsertProjectEventContactContext(sp_ProjectEventContact_DM InputProjectEventContact);
+        void UpdateProjectEventContactContext(sp_ProjectEventContact_DM InputProjectEventContact);
+        void DeleteProjectEventContactContext(sp_ProjectEventContact_DM InputProjectEventContact);
+    }
+}
